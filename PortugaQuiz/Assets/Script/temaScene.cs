d973d174f6099b0830fb7c31ffe17a2449bfb0b6
IDouglasSl <israeldouglasster@gmail.com>
2024-01-17 19:25:53 -0300

    Desenvolvimento da paginação de Temas
    Foi desenvolvido a paginação na cena de temas. Pode ser criado quantos painéis de temas necessário, basta só inclui-los  no gamecontroller

--- PortugaQuiz/Assets/Script/temaScene.cs
+++ PortugaQuiz/Assets/Script/temaScene.cs
@@ -10,10 +10,48 @@ public class temaScene : MonoBehaviour
     public Text         nomeTemaTxt;
     public Button       btnJogar;
 
+    [Header("Configuração da páginação")]
+    public GameObject[]     btnPaginacao;
+    public GameObject[]     Paineltemas;
+    private bool            ativarbtnPaginacacao;
+    private int             idPagina;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        onOffButtonsAndPaineis();
+
+    }
+
+    void onOffButtonsAndPaineis()
+    {
         btnJogar.interactable = false; // o botão de jogar inicial inativo
+
+        foreach(GameObject p in Paineltemas)
+        {
+            p.SetActive(false);
+        }
+
+        Paineltemas[0].SetActive(true);
+
+
+        if (Paineltemas.Length > 1)
+        {
+            ativarbtnPaginacacao = true;
+        }
+        else
+        {
+            ativarbtnPaginacacao = false;
+        }
+
+
+        foreach(GameObject b in btnPaginacao)
+        {
+            
+            b.SetActive(ativarbtnPaginacacao);
+        }
+
     }
 
     public void jogar()
@@ -26,6 +64,29 @@ public class temaScene : MonoBehaviour
     }
 
 
+    public void btnPagina(int i)
+    {
+
+
+        idPagina += i;
+
+        if(idPagina < 0) { idPagina = Paineltemas.Length - 1; }
+        else if(idPagina >= Paineltemas.Length) { idPagina = 0; }
+
+
+        btnJogar.interactable = false;
+        nomeTemaTxt.color = Color.white;
+        nomeTemaTxt.text = "Selecione um tema";
+
+        foreach (GameObject p in Paineltemas)
+        {
+            p.SetActive(false);
+        }
+
+        Paineltemas[idPagina].SetActive(true);
+
+
+    }
 
 
 }
